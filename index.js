02dacc9c084feafbf46f86174df8fc9a590e57a7
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-04-20 18:09:26 -0300

    fix route por mygames

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const server = require('./src/app.js');
-const { fetchGenres , fetchPlatforms} = require('./src/Helpers/index');
-const { conn , Genre , Platform } = require('./src/db.js')
+const server = require("./src/app.js");
+const { fetchGenres, fetchPlatforms } = require("./src/Helpers/index");
+const { conn, Genre, Platform } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-    
   server.listen(3001, async () => {
-    const genresFetch = await fetchGenres().then(({ data : { results }}) =>  results)
-    const platformsFetch = await fetchPlatforms().then(({ data : { results }}) =>  results)
-       
+    const genresFetch = await fetchGenres().then(
+      ({ data: { results } }) => results
+    );
+    const platformsFetch = await fetchPlatforms().then(
+      ({ data: { results } }) => results
+    );
 
+    const genres = genresFetch.map(({ name }) => {
+      Genre.findOrCreate({ where: { name: name } });
+    });
 
-   const genres =  genresFetch.map(({name}) => {
-      Genre.findOrCreate({where: {name : name} })
-      })
-    
-    
-    const platforms = platformsFetch.map(({name , image_background}) => {
-      Platform.findOrCreate({where: {name,  img: image_background} })
-      })
-      
-      
+    const platforms = platformsFetch.map(({ name, image_background }) => {
+      Platform.findOrCreate({ where: { name, img: image_background } });
+    });
 
-    console.log('DB listening at PORT : 3001'); // eslint-disable-line no-console
+    console.log("DB listening at PORT : 3001"); // eslint-disable-line no-console
   });
 });
